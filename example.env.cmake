8b2092ec6479f9e3afb8eeb68caa44073b608de2
EmreDogann <emre.dogan.1@protonmail.com>
2022-02-08 15:05:16 +0000

    Updated .gitignore and added example .env.cmake file

---
+++ example.env.cmake
@@ -0,0 +1,8 @@
+# YOUR VULKAN PATH HERE.
+# Example: C:/VulkanSDK/1.2.189.2
+set(VULKAN_SDK_PATH  "")
+
+# YOUR MINGW-W64 PATH HERE. WITHOUT /BIN
+# Example 1: C:/Users/Emre/Downloads/mingw64
+# Example 2: C:/msys64/mingw64
+set(MINGW_PATH "")
