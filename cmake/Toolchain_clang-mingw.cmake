589ae7729f5279f4efc6e93ef42d7614a3b82841
EmreDogann <emre.dogan.1@protonmail.com>
2022-03-24 06:26:13 +0000

    Drastically improved compile times.

---
+++ cmake/Toolchain_clang-mingw.cmake
@@ -0,0 +1,30 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Windows)
+
+# Don't run the linker on compiler check
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(UNITY_BUILD ON)
+set(UNITY_BUILD_BATCH_SIZE 0)
+
+# set(CMAKE_SYSROOT "C:/msys64/mingw64")
+
+set(CMAKE_C_COMPILER "C:/msys64/clang64/bin/clang.exe")
+set(CMAKE_CXX_COMPILER "C:/msys64/clang64/bin/clang++.exe")
+
+set(CMAKE_C_COMPILER_TARGET x86_64-windows-gnu)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-windows-gnu)
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
